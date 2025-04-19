3add350fa5223be3a45260a01d6f7162892db946
Ayaad72 <ayaadalavi@gmail.com>
2025-04-20 00:50:14 +0500

    latest changes

--- components/shared/cta-section.tsx
+++ components/shared/cta-section.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-// Hey sadasda is dasdasda call to action section
+// Hey sadasda is sdasdasdasasasda call to action section
 interface CTASectionProps {
   title: string;
   description: string;
