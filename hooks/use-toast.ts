51d8b3c5f2281c080cfa9f7333c33bd485a593c6
Ayaad72 <ayaadalavi@gmail.com>
2025-04-22 19:48:59 +0500

    pushing

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,6 +1,6 @@
 "use client";
 
-// sadfasdasda by sdsadasd-hot-toast library
+// sadfasdasda by react-hot-toast library
 import * as React from "react";
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
