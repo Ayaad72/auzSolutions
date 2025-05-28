04cac399b4f75023607f085d435e45109e6597b6
Ayaad72 <ayaadalavi@gmail.com>
2025-05-29 01:56:02 +0500

    changes

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -26,7 +26,7 @@ const AlertDialogOverlay = React.forwardRef<
   />
 ));
 AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName;
-// dasdasdasdsa
+// asdasdasdas
 const AlertDialogContent = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
